972e5326ebe929022123bfb6f1a7d36dd83e8f53
AlesRch <ales.roch@seznam.cz>
2024-05-13 20:35:49 +0200

    Domácí úkol 1.2.2
    Jedná se o domácí úkol z první lekce, kde jsem si i pokoušel přidat datum narození, ale nevím, jak správně zadat formát.

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,23 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import com.engeto.seller;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("Hello and welcome!");
+
+        seller seller = new seller("Aleš Roch", 16.05.1986, 15, 150, "Prague", "8YES5678", 8.6, "192.168.1.123");
+
+        System.out.println(seller.getNameAndSurname());
+        System.out.println(seller.getBirthDate());
+        System.out.println(seller.getNumberOfConcludedContrats() + " concluded contracts");
+        System.out.println(seller.getQuantity() + " tons");
+        System.out.println(seller.getAddress());
+        System.out.println(seller.getLicensePlateNumber());
+        System.out.println(seller.getConsumption() + " l/100Km");
+        System.out.println(seller.getiPAddress());
+
+        double avarageQuantityPerContract = (seller.getQuantity() / seller.getNumberOfConcludedContrats());
+        System.out.println("avarage quantity per contract is: " + avarageQuantityPerContract + " tons");
+
         }
     }
-}
--- src/com/engeto/seller.java
+++ src/com/engeto/seller.java
@@ -1,2 +1,92 @@
-package com.engeto;public class seller {
+package com.engeto;
+
+import java.time.LocalDate;
+
+public class seller {
+    private String nameAndSurname;
+    private LocalDate birthDate;
+    private int numberOfConcludedContrats;
+    private double quantity;
+    private String address;
+    private String licensePlateNumber;
+    private double consumption;
+    private String iPAddress;
+
+    public seller(String nameAndSurname, LocalDate birthDate,
+                  int numberOfConcludedContrats, double quantity,
+                  String address, String licensePlateNumber,
+                  double consumption, String iPAddress) {
+        this.nameAndSurname = nameAndSurname;
+        this.birthDate = birthDate;
+        this.numberOfConcludedContrats = numberOfConcludedContrats;
+        this.quantity = quantity;
+        this.address = address;
+        this.licensePlateNumber = licensePlateNumber;
+        this.consumption = consumption;
+        this.iPAddress = iPAddress;
+    }
+
+    public String getNameAndSurname() {
+        return nameAndSurname;
+    }
+
+    public void setNameAndSurname(String nameAndSurname) {
+        this.nameAndSurname = nameAndSurname;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate() {
+        this.birthDate = birthDate;
+    }
+
+    public int getNumberOfConcludedContrats() {
+        return numberOfConcludedContrats;
+    }
+
+    public void setNumberOfConcludedContrats(int numberOfConcludedContrats) {
+        this.numberOfConcludedContrats = numberOfConcludedContrats;
+    }
+
+    public double getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(double quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getLicensePlateNumber() {
+        return licensePlateNumber;
+    }
+
+    public void setLicensePlateNumber(String licensePlateNumber) {
+        this.licensePlateNumber = licensePlateNumber;
+    }
+
+    public double getConsumption() {
+        return consumption;
+    }
+
+    public void setConsumption(double consumption) {
+        this.consumption = consumption;
+    }
+
+    public String getiPAddress() {
+        return iPAddress;
+    }
+
+    public void setiPAddress(String iPAddress) {
+        this.iPAddress = iPAddress;
+    }
 }
